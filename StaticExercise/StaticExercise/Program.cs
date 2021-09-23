9802d87d5238f893b6057aef75a39fea125334b3
jsalaiz <jonathan.salaiz@gmail.com>
2021-09-22 21:27:29 -0400

    Completed the exercise

---
+++ StaticExercise/StaticExercise/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"32 degrees Fahrenheit in Celsius is: {TempConverter.FahrenheitToCelsius(32)}");
+            Console.WriteLine($"100 degrees Celsius in Fahrenheit is: {TempConverter.CelsiusToFahrenheit(100)}");
+        }
+    }
+}
---
+++ StaticExercise/StaticExercise/TempConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double f)
+        {
+            double c = (f - 32) * (5/9);
+            return c;
+        }
+
+        public static double CelsiusToFahrenheit(double c)
+        {
+            double f = (c * 1.8) + 32;
+            return f;
+        }
+    }
+}
